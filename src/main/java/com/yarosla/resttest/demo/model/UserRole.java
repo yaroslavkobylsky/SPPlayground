aab74f75afad3009873c5a0b15de142d796740c7
yaroslav kobylskiy <yaroslav.kobylsky@ctdev.io>
2020-04-12 22:58:44 +0300

    SBPlayground: add user role entity

---
+++ src/main/java/com/yarosla/resttest/demo/model/UserRole.java
@@ -0,0 +1,31 @@
+package com.yarosla.resttest.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
